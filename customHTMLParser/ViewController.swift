d232f4cacfba807b2f13b9be48ac3094947347a6
LV <vvvlizagub@gmail.com>
2021-09-29 12:10:34 +0300

    a

--- customHTMLParser/ViewController.swift
+++ customHTMLParser/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterTextView: UITextView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func convertButtonDidTap(_ sender: Any) {
+        let str = enterTextView.text
+        if (str != "") {
+            let tmp = HTMLParser().htmlToString(str!)
+            resultLabel.attributedText = NSAttributedString(string: "Hi")
+            resultLabel.attributedText = tmp
+        }
+        else {
+            resultLabel.text = "empty string"
+        }
+        
+    }
 }
 
